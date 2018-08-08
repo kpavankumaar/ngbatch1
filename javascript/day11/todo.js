a174686dd3b5f35323b4e9190362747ebef2a6d6
prudhvirajdandi <prudhvirajdandi@gmail.com>
2018-08-08 09:29:00 +0530

    todoapp and form validaton

--- javascript/day11/todo.js
+++ javascript/day11/todo.js
@@ -1,5 +1,6 @@
 var addBtn = document.getElementsByTagName('button')[0];
 var incompleteTask = document.getElementById('incompleteTask');
+var addTaskInputField = document.getElementsByName('addtask')[0];
 function addTask(){
 	// create li , input checkbox ,input text , label ,edit button and delete button
 	var taskItem = document.createElement('li');
@@ -8,9 +9,15 @@ function addTask(){
 	var showTaskEl = document.createElement('label');
 	var editBtn = document.createElement('button');
 	var deleteBtn = document.createElement('button');
-	var arr = [checkBox,inputTextEl,showTaskEl,editBtn, deleteBtn];
+	var arr = [checkBox,inputTextEl,showTaskEl,editBtn, deleteBtn]
 	
 	checkBox.setAttribute('type','checkbox');
+	inputTextEl.setAttribute('type','text');
+	showTaskEl.innerText = addTaskInputField.value;
+	editBtn.innerText = 'Edit';
+	deleteBtn.innerText = 'Delete';
+
+
 	arr.map(function(element){
 		taskItem.appendChild(element);	
 	})
