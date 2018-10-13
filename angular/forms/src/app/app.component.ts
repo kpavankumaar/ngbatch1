dac40a2b58972fae1cefa661317f917d662c64f2
nbitsmeanstack49 <nbitsmeanstack49@gmail.com>
2018-10-13 10:33:10 +0530

    reactive forms

--- angular/forms/src/app/app.component.ts
+++ angular/forms/src/app/app.component.ts
@@ -1,13 +1,40 @@
 import { Component } from '@angular/core';
-import { FormControl} from '@angular/forms'
+import { FormControl, FormGroup, Validators, FormBuilder} from '@angular/forms'
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+      constructor(private fb:FormBuilder){
+        
+      }
       name = new FormControl('sneha');
+
       update(){
         this.name.setValue('ravi');
+      }
+      profileForm = this.fb.group({
+        firstname: [''],
+        lastname: [''],
+        phonenumber: [''],
+        address: this.fb.group({
+          city: [''],
+          state: ['']
+        })
+        
+      })
+      // profileForm = new FormGroup({
+      //   firstname: new FormControl(''),
+      //   lastname: new FormControl(''),
+      //   phonenumber: new FormControl(''),
+      //   address: new FormGroup({
+      //     city: new FormControl(''),
+      //     state: new FormControl('')
+      //   })
+        
+      // })
+      onSubmit(value){
+        console.log(value);
       }
 }
